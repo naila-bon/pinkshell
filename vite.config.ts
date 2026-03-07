bb50f9d9f0c7ae9977d47283a78cdef5d8df9d1f
Naila Bon <153430078+naila-bon@users.noreply.github.com>
2026-03-07 15:45:23 +0000

    add provider

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/pinkshell/', // nom exact de ton repo
+  plugins: [react(), tsconfigPaths()],
+  base: '/pinkshell/', 
 })
